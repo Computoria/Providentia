bd88668d0164cf76cf97dac35d162f950e72ef5e
Data Spaz <Data Spaz@Spazbot>
2016-04-16 21:12:51 -0500

    Setup

---
+++ providentia-engine/src/main/java/org/computoria/providentia/engine/Driver.java
@@ -0,0 +1,10 @@
+package org.computoria.providentia.engine;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ providentia-engine/src/main/java/org/computoria/providentia/engine/Physical.java
@@ -0,0 +1,5 @@
+package org.computoria.providentia.engine;
+
+public class Physical {
+
+}
